779d240d80c5ef70c933244e18a93ff787a4522a
Classified_User 47 <66116118+AryamanAdivyaSingh47@users.noreply.github.com>
2022-10-08 22:47:04 +0530

    Time: 73 ms (99.26%), Space: 26.9 MB (18.03%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,23 @@
+class MyCalendarThree {
+    map<int,int>calendar = {{-1,0}};
+    int res = 0;
+public:
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int s, int e) {
+       auto start = calendar.emplace(s, (--calendar.upper_bound(s))->second);
+        auto end = calendar.emplace(e, (--calendar.upper_bound(e))->second);
+        for (auto i = start.first; i != end.first; ++i)
+            res = max(res, ++(i->second));
+        return res;
+        
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
